c35cca8df2e91ae22da25c76f0e3991d1b549daa
omarmhaimdat <omarmhaimdat@Mhaimdats-MBP.lan>
2017-11-07 22:21:38 +0000

    Ajout du code de triangulation, il reste à generaliser ce code

--- Matrices/Matrices/Program.cs
+++ Matrices/Matrices/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TriangulationSysteme;
+using InputOutputMatrice;
 
 namespace Matrices
 {
@@ -6,7 +8,24 @@ namespace Matrices
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Probleme du cours de Ramadane
+            double[,] A = { { 2, 3, -1 }, { 4, 4, -3 }, { -2, 3, -1 } };
+            double[] B = { 5, 3, 1 };
+
+            //Probleme trouver sur internet
+            //double[,] A = { { 2, 1, -4 }, {3 , 3, -5 }, { 4, 5, -2 } };
+            //double[] B = { 8, 14, 16};
+
+            Console.WriteLine("La matrice avant changement: ");
+            Input.OutputMatrice(A);
+
+            Console.WriteLine("La matrice U apres changement: ");
+            Input.OutputMatrice(ClassPivotGauss.TriangulaireSuperieur(A));
+
+            Console.WriteLine("La matrice L apres changement: ");
+            Input.OutputMatrice(ClassPivotGauss.TriangulaireInferieur(A));
+
+            //Input.OutputMatriceB((ClassPivotGauss.PivotGaussA(A, B)));
         }
     }
 }
